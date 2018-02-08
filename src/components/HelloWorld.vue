352fcca95374671d9f90707b66b757bfdb70c4c5
Fea-Sin <1210992614@qq.com>
2018-02-08 14:51:06 +0800

    commit first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    快递
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .hello {
+    font-size: 20px;
+    color: antiquewhite;
+  }
+</style>
+
